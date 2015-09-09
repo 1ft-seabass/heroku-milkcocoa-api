614b124eddeacbb72e2a7abcc68ae8ebd52c937f
Tanaka Seigo <seigo@1ft-seabass.jp>
2015-09-10 02:07:07 +0900

    初回コミット ・Heroku用のapp.jsを整備 ・README.mdを用意 ・基本的な連携できるソースコードを用意
    Signed-off-by: Tanaka Seigo <seigo@1ft-seabass.jp>

---
+++ README.md
@@ -0,0 +1,9 @@
+# NodeJS Milkcocoa Heroku Button
+
+HerokuでNodeJS(Express)をベースにした、シンプルなMilkcocoa連携サーバーです。
+
+以下のHeroku Buttonを押してHerokuアプリを作成しましょう。
+
+## Heroku Button
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/1ft-seabass/heroku-milkcocoa-api)
---
+++ index.js
@@ -0,0 +1,56 @@
+// JSONファイルの読み込み（ローカル用）/////////////////////////////////
+var fs = require('fs');
+var setting = {};
+var MILKCOCOA_APP_ID = "";
+var MILKCOCOA_DATASTORE_ID = "";
+if( process.env.PORT ) {
+    // Heroku上では環境変数から読み込む（インストール時に設定）
+    MILKCOCOA_APP_ID = process.env.MILKCOCOA_APP_ID;
+    MILKCOCOA_DATASTORE_ID = process.env.MILKCOCOA_DATASTORE_ID;
+} else {
+    // .envフォルダはあらかじめ .gitignore 対象にしておく。
+    setting = JSON.parse(fs.readFileSync('.env/setting.json', 'utf8'));
+    //
+    MILKCOCOA_APP_ID = setting.MILKCOCOA_APP_ID;
+    MILKCOCOA_DATASTORE_ID = setting.MILKCOCOA_DATASTORE_ID;
+}
+
+console.log("MILKCOCOA_APP_ID:" + MILKCOCOA_APP_ID);
+console.log("MILKCOCOA_DATASTORE_ID:" + MILKCOCOA_DATASTORE_ID);
+
+// milkcocoa /////////////////////////////////
+var MilkCocoa = require("./node_modules/milkcocoa/index.js");
+var milkcocoa = new MilkCocoa(MILKCOCOA_APP_ID + ".mlkcca.com");
+// dataStore作成 デフォルトのデータストアIDは heroku_sample にしています。
+var sampleDataStore = milkcocoa.dataStore(MILKCOCOA_DATASTORE_ID);
+// データがpushされたときのイベント通知
+sampleDataStore.on("push", function(datum) {
+    // 内部のログ
+    console.log('[push complete]');
+    console.log(datum);
+});
+//////////////////////////////////////////////
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+    response.send('Hello Milkcocoa!');
+});
+
+app.get('/push', function(request, response) {
+    response.send('[push datastore]');
+    // 内部のログ
+    console.log('[push datastore : ' + MILKCOCOA_DATASTORE_ID + ']');
+    sampleDataStore.push({ text : "送信テスト" , host : request.headers.host });
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
